132057cc6b35039259fe57f32702e5aaec5a514e
honlzl <441150781@qq.com>
2023-12-15 22:26:23 +0800

    1

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/ai/Cplusplus/simple_rpc/rocket/common/log.cc" "CMakeFiles/app.dir/rocket/common/log.cc.o" "gcc" "CMakeFiles/app.dir/rocket/common/log.cc.o.d"
+  "/data/ai/Cplusplus/simple_rpc/rocket/common/util.cc" "CMakeFiles/app.dir/rocket/common/util.cc.o" "gcc" "CMakeFiles/app.dir/rocket/common/util.cc.o.d"
+  "/data/ai/Cplusplus/simple_rpc/testcases/test_log.cc" "CMakeFiles/app.dir/testcases/test_log.cc.o" "gcc" "CMakeFiles/app.dir/testcases/test_log.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
